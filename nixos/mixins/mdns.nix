1b979a5543e5458a9167592355c1f18c55bfe0e8
Jörg Thalheim <joerg@thalheim.io>
2025-01-11 11:52:42 +0100

    fix filtering

--- nixos/mixins/mdns.nix
+++ nixos/mixins/mdns.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }:
 let
-  dhcpInterfaces = lib.filterAttrs (i: i.useDHCP == true) config.networking.interfaces;
+  dhcpInterfaces = lib.filterAttrs (_: i: i.useDHCP == true) config.networking.interfaces;
 in
 {
   # Avahi is an alternative implementation. If it's enabled, than we don't need the code below.
